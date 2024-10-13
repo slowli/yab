2bef6af3ed57970374cd8536dfd262381a1324f8
slowli <slowli@users.noreply.github.com>
2024-10-13 13:15:23 +0000

    Deploying to gh-pages from @ slowli/yab@01e8ed2e4c4021bacdddfaa25f8a6e92b3439a9a 🚀

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"yab":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"yab/struct.BenchmarkId.html\" title=\"struct yab::BenchmarkId\">BenchmarkId</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
