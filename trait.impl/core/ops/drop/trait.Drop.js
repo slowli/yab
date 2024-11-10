b420b86364520fbf37bc2d6984f9b2737ec16634
slowli <slowli@users.noreply.github.com>
2024-11-10 15:04:58 +0000

    Deploying to gh-pages from @ slowli/yab@9e3f2e976bb3dd8c9f01cb81bbd97768a70c6d6f 🚀

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"yab":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"yab/struct.CaptureGuard.html\" title=\"struct yab::CaptureGuard\">CaptureGuard</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
