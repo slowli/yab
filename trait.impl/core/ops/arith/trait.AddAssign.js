037a7cb44e67fe3e7cb1e378071085bc9b323e73
slowli <slowli@users.noreply.github.com>
2025-06-01 08:22:19 +0000

    Deploying to gh-pages from @ slowli/yab@fbc3aa6cc280da4a8eddd339157784206cc3510e 🚀

---
+++ trait.impl/core/ops/arith/trait.AddAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["yab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"enum\" href=\"yab/enum.CachegrindStats.html\" title=\"enum yab::CachegrindStats\">CachegrindStats</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[291]}
---
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["yab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"yab/struct.CachegrindFunction.html\" title=\"struct yab::CachegrindFunction\">CachegrindFunction</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[302]}
